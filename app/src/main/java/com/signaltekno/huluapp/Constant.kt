a532f7432c5a942f9eb0b7dd453196d28a1371c2
dennykurniawan48 <kurniawan.denny.48.com>
2021-12-20 06:03:45 +0700

    initial commit

--- app/src/main/java/com/signaltekno/huluapp/Constant.kt
+++ app/src/main/java/com/signaltekno/huluapp/Constant.kt
@@ -4,7 +4,7 @@ import com.signaltekno.huluapp.model.GenreData
 import com.signaltekno.huluapp.navigation.Screen
 
 object Constant {
-    val API_KEY = "ce13b578f8c52b4310e95a8f28255cd5"
+    val API_KEY = ""
     val URL_ASSET = "https://image.tmdb.org/t/p/original"
     val LIST_GENRE = listOf(GenreData("Trending", "https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en"),
         GenreData("Top Rated", "https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en"),
